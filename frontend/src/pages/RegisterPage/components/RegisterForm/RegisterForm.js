7e5055d7d11a1b0b4db8682f6a2ce6890c52da91
Paweł Korytowski <pkorytowski@hotmail.com>
2020-10-11 22:55:00 +0200

    Connect to db. Connect frontend and backend

--- frontend/src/pages/RegisterPage/components/RegisterForm/RegisterForm.js
+++ frontend/src/pages/RegisterPage/components/RegisterForm/RegisterForm.js
@@ -14,11 +14,24 @@ const RegisterForm = () => {
         setPassword('')
         
     }
+
+    const register = async () => {
+        const url = 'http://localhost:4000/register';
+        const data = {Name: login, Password: password};
+
+        const response = await fetch(url,{
+            method: 'POST',
+            credentials: 'omit',
+            headers: {'Content-Type': 'application/json'},
+            body: {name: "login", password: "password"}
+        });
+        //console.log(response);
+    }
     return (
         <form onSubmit={handleLogin} className={styles.RegisterForm}>
         <label><input type="text" value={login} onChange={e => setLogin(e.target.value)}/></label>
-        <label><input type="date" value={password} onChange={e => setPassword(e.target.value)}/></label>
-        <button type="submit">add</button>
+        <label><input type="text" value={password} onChange={e => setPassword(e.target.value)}/></label>
+        <button type="submit" onClick={register}>add</button>
     </form>
 
     )
