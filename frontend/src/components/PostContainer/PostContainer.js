06011e9aa964ccfebd4351f73820f2faaf9eb06f
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-10-12 19:58:58 +0200

    some changes

--- frontend/src/components/PostContainer/PostContainer.js
+++ frontend/src/components/PostContainer/PostContainer.js
@@ -1,8 +1,22 @@
 import React from 'react';
-import styles from './PostContainer.module.css'; 
+import styles from './PostContainer.module.css';
+import Slider from 'react-animated-slider';
+import 'react-animated-slider/build/horizontal.css';
+
 const PostContainer = () => {
-    return (
-       <div className={styles.PostContainer}>PostContainer</div>
+    const slides = [
+        { title: 'First item', description: 'Lorem ipsum'},
+        { title: 'Second item', description: 'Lorem ipsum'}
+      ];
+    return ( 
+      <div className={styles.PostContainer}>
+      <Slider>
+  {slides.map((slide, index) => <div key={index}>
+    <h2>{slide.title}</h2>
+    <div>{slide.description}</div>
+  </div>)}
+</Slider>
+</div>
     )
 }
 export default PostContainer;
