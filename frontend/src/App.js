304ddf735d2b2af550a1f13a262f9268e97e76a6
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-10-13 11:41:59 +0200

    after login option to logout/account instead  of login/register

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import {Page} from 'components'
 function App() {
+  const [isLogged, setLogged] = useState(false)
   return (
     <div className="App">
       <Router>
       <Switch>
           <Route path="*"> 
-              <Page />
+              <Page setLogged={setLogged} isLogged={isLogged} />
           </Route>
         </Switch>
       </Router>
