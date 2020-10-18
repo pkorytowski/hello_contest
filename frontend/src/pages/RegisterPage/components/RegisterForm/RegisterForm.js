25aa34ba298de7fb6fcf1621d8d0b7b392f29553
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-10-18 15:01:07 +0200

    clear code in register code

--- frontend/src/pages/RegisterPage/components/RegisterForm/RegisterForm.js
+++ frontend/src/pages/RegisterPage/components/RegisterForm/RegisterForm.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import styles from './RegisterForm.module.css';
 import { useForm } from "react-hook-form";
-import {Link, Redirect, useHistory} from "react-router-dom";
+import {Redirect, useHistory} from "react-router-dom";
 const RegisterForm = () => {
     const { register, handleSubmit, formState} = useForm();
     const [isRegister, setRegister] = useState(false)
@@ -28,17 +28,17 @@ const RegisterForm = () => {
         <form onSubmit={handleSubmit(handleRegister)} className={styles.RegisterForm}>
             <h3 className={styles.rform}>Registration Form</h3>
             <label >Name </label>
-            <input ref={register} name="name" type="text" /><br/>
+            <input ref={register} name="name" type="text" />
             <label>Surname</label>
-            <input ref={register} name="surname" type="text" /><br/>
+            <input ref={register} name="surname" type="text" />
             <label>Your born date:</label>
-            <input ref={register} name="born_date"  type="date" /><br/>
+            <input ref={register} name="born_date"  type="date" />
             <label>Email</label>
-            <input ref={register} name="email"  type="text" /><br/>
+            <input ref={register} name="email"  type="text" />
             <label>Nickname</label>
-            <input ref={register} name="user_name" type="text" /><br/>
+            <input ref={register} name="user_name" type="text" />
             <label>Password </label>
-            <input ref={register} name="password" type="password" /><br/>
+            <input ref={register} name="password" type="password" />
         <div className={styles.btn}>
         <button type="button" className={styles.register} onClick={() => push('/')}>To main</button>
         <button type="submit" className={styles.register} disabled={numberOfInputsInForm()} >register now</button>
