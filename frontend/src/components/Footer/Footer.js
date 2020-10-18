ec9fa8fb5c8198c09479faaf914edc14cb8eedc3
Michal Kacprzak <michal.kacprzak999@gmail.com>
2020-10-18 11:35:34 +0200

    update footer

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -2,7 +2,11 @@ import React from 'react';
 import styles from './Footer.module.css'; 
 const Footer = () => {
     return (
-       <div className={styles.Footer}>Footer</div>
+       <div className={styles.Footer}>
+           <span>Copyright {new Date().getFullYear()} &copy; - Hello Contest </span>
+           <span>Privacy policy </span>
+           <span>Terms of use </span>
+        </div>
     )
 }
 export default Footer;
